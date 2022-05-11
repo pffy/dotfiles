8a3a81edf708201ee815014a17bd58bff5926488
The Pffy Authors <50888935+pffy@users.noreply.github.com>
2022-05-11 14:16:16 -0700

    Create comments.php

---
+++ code/comments.php
@@ -0,0 +1,9 @@
+<?php
+
+## name     : class-pffy-hello.php
+## task     : PHP implementation of the Hello object
+## version  : 1
+## update   : 2022-02-22
+## git      : https://github.com/pffy/dotfiles
+## license  : https://opensource.org/licenses/MIT
+## notes    : add context to code
