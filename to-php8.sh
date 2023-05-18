c6b6f69cb7f6ce99ebb3235b52cf5e276923356e
The Pffy Authors <50888935+pffy@users.noreply.github.com>
2023-05-18 10:10:34 -0700

    Create to-php8.sh

---
+++ to-php8.sh
@@ -0,0 +1,12 @@
+## updatinga apt repos to PHP 8, using the solution from:
+## https://php.watch/articles/php-8.0-installation-update-guide-debian-ubuntu
+
+sudo apt install apt-transport-https lsb-release ca-certificates wget -y
+sudo wget -O /etc/apt/trusted.gpg.d/php.gpg https://packages.sury.org/php/apt.gpg 
+sudo sh -c 'echo "deb https://packages.sury.org/php/ $(lsb_release -sc) main" > /etc/apt/sources.list.d/php.list'
+sudo apt update
+
+sudo apt install php8.0-common php8.0-cli -y
+
+php -m > phpm.txt
+php -v > phpv.txt
